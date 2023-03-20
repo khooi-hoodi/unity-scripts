eba793d012a481da669a73c0b521f6aab7369b54
Khoi Tran <khooi.hoodi@gmail.com>
2023-03-20 22:51:31 +0700

    first commit

---
+++ Learning.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Learning : MonoBehaviour
+{
+    // variables
+    // double mana = 15.5;
+    // float speed = 5f;
+    // int health = 100;
+    // string playerName = "Warrior";
+    // bool isDead = false;
+    // char oneChar = 'a'; // optional
+    // end of main variables 
+
+    void Start()
+    {
+        print("this is from print()");
+
+        Debug.Log("this is from Debug.Log()");
+    }
+}
